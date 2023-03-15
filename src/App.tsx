0ce3953a7bd8bd8f5a7fcd72c77a1a5596575bde
ju-voro <voronetskajaj@hotmail.com>
2023-03-15 22:24:49 +0100

    local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState<number>(0);
+
+  useEffect(() => {
+    let valueAsString = localStorage.getItem('counterValue');
+    if(valueAsString) {
+      let newValue = JSON.parse(valueAsString);
+      setValue(newValue);
+    }
+  }, []);
+
+  useEffect(()=> {
+    localStorage.setItem('counterValue', JSON.stringify(value));
+  }, [value]);
+
+  const incHandler = () => {
+    setValue(value + 1);
+
+  }
+
+  // const setToLocalStorageHandler = () => {
+  //   localStorage.setItem('counterValue', JSON.stringify(value));
+  //   localStorage.setItem('counterValue + 1', JSON.stringify(value + 1));
+  // }
+
+  // const getFromLocalStorageHandler = () => {
+  //   let valueAsString = localStorage.getItem('counterValue');
+  //   if(valueAsString) {
+  //     let newValue = JSON.parse(valueAsString);
+  //     setValue(newValue);
+  //   }
+  // }
+
+  // const clearLocalStorageHandler = () => {
+  //   localStorage.clear();
+  //   setValue(0);
+  // }
+
+  // const removeFromLocalStorageHandler = () => {
+  //   localStorage.removeItem('counterValue + 1');
+  // }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>LocalStorage</h2>
+      <h1>{value}</h1>
+      <button onClick={incHandler}>inc</button>
+      {/* <div>
+
+        <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+        <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+        <button onClick={clearLocalStorageHandler}>ClearLocalStorage</button>
+        <button onClick={removeFromLocalStorageHandler}>removeFromLocalStorage</button>
+      </div> */}
+      
     </div>
   );
 }
